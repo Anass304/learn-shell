f0223c53717c1523257a4640285409eff305b180
Anass <shaikanass304@gmail.com>
2023-05-06 15:49:57 +0530

    Printing statements

---
+++ 01-sample.sh
@@ -0,0 +1,15 @@
+# learning-shell
+# here we are leaning shell scripting
+# shell scripting topics
+...text
+1.printing
+2.comments
+3.variables
+4.Functions
+5.Conditions
+6.Loops
+7.Inputs
+8.Exit status
+9.Quotes
+10.Redirectories
+...
