c9476b8891259bf79a4ce7b92b651a42bb7dec47
Anass <shaikanass304@gmail.com>
2023-05-06 16:13:37 +0530

    Printing a colurs

--- 02-print.sh
+++ 02-print.sh
@@ -10,14 +10,22 @@ echo "**** Hello World ****"
 # Magenta   - 35 - echo -e "\e[35mMESSAGE\e[0m"
 # Cyan      - 36 - echo -e "\e[36mMESSAGE\e[0m"
 
-#echo -e : enable colour from echo cmd
-#\e : enable a particular colour
-#\e[0m : zero code is going to reset the colour , if we eanble it we need to diable otherwise colour will continue
 
 
-echo -e "\e[31mMESSAGE\e[0m"
-echo -e "\e[32mMESSAGE\e[0m"
-echo -e "\e[33mMESSAGE\e[0m"
-echo -e "\e[34mMESSAGE\e[0m"
-echo -e "\e[35mMESSAGE\e[0m"
-echo -e "\e[36mMESSAGE\e[0m"
+
+# Enable Color
+# Syntax - echo -e "\e[COLmMESSAGE\e[0m"
+# -e - enable \e (Enable color in echo command)
+# All the input should be there in quotes (Double quotes preferred)
+# \e - Enable a color
+# COLm - Color Code - Red(31m), Green(32m), Yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
+# \e[0m - Disable the color. (Once we enable the color we have to diable that, else color folloows to next lines)
+
+
+echo -e "\e[31mRed color \e[0m"
+echo -e "\e[32mGreen color\e[0m"
+echo -e "\e[33mYellow color\e[0m"
+echo -e "\e[34mBlue color\e[0m"
+echo -e "\e[35mMagenta color\e[0m"
+echo -e "\e[36mCyan color\e[0m"
+echo -e Normal Text
