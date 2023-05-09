356bf693662606fbdfe29d1dee225739aa49e4c7
Anass <shaikanass304@gmail.com>
2023-05-09 11:09:01 +0530

    loops

--- 09-loops.sh
+++ 09-loops.sh
@@ -12,3 +12,19 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
   echo $i
   sleep 1
 done
+
+#another example
+a=10
+
+while [ $a -gt 0 ]; do
+  echo a - $a - greater than 0
+  a=$(($a-1))
+  sleep 1
+done
+
+# If expression is true then loop allows inside.
+
+for fruit in apple banana orange ; do
+  echo Fruit Name - $fruit
+  sleep 1
+done
