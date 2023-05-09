c3c4d5020bc3d7c9d3d28bb03085dab5af20dc13
Anass <shaikanass304@gmail.com>
2023-05-09 10:49:11 +0530

    Shell Scripting Topics

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,15 +1,11 @@
-# learning-shell
-# here we are leaning shell scripting
-# shell scripting topics
-...text
-1.printing
-2.comments
-3.variables
-4.Functions
-5.Conditions
-6.Loops
-7.Inputs
-8.Exit status
-9.Quotes
-10.Redirectories
-...
+#!/bin/bash
+#!/usr/bin/env bash
+
+# List files
+ls
+
+# cat user file
+cat /etc/passwd | head -2
+
+# grep root user from user file
+grep root /etc/passwd
