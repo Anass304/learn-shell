68be9d874f047e3ad06034d1416cf6c4f0e490de
Anass <shaikanass304@gmail.com>
2023-05-09 10:48:33 +0530

    Shell Scripting Topics

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,2 +1,17 @@
-DATE=2023-05-07
-echi "Hello , good morning , today date is ${DATE}"
+## If we assign a name to a set of data is called as variable.
+
+name=Anass
+designation =Software Engineer
+
+echo Name - $name
+echo Designation- ${designation}
+
+#DATE=2023-05-07
+DATE=$(date +%F)
+echo Hey, Today date is $DATE
+
+ADD=$((2+3+4/5))
+echo ADD - $ADD
+
+## Variable from command line
+echo b - $b
