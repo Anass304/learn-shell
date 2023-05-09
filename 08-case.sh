8a7681f0a76fb83d86d845f46e7b104e5ac8e632
Anass <shaikanass304@gmail.com>
2023-05-09 11:06:37 +0530

    All Topics

--- 08-case.sh
+++ 08-case.sh
@@ -4,6 +4,24 @@
 # *) commands ;;
 # esac
 
+fruit=$1
+
+case $fruit in
+  apple)
+    echo Price - 10$
+    ;;
+  banana)
+    echo print - 0.2$
+    ;;
+  *)
+    echo Fruit not found
+    ;;
+esac
+
+# We dont prefer case condition, because it has limitatons as it can do only string comparisons.
+
+#another examples
+
 read -p 'Enter Course Name: ' name
 
 case $name in
