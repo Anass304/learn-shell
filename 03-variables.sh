37b90c03b11a59d19a849658cd2a939e9d20c69e
Anass <shaikanass304@gmail.com>
2023-05-09 10:59:54 +0530

    source common

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 ## If we assign a name to a set of data is called as variable.
-
+a=10
 name=Anass
 designation =Software Engineer
 
+
+# Print Variables
 echo Name - $name
 echo Designation- ${designation}
 
@@ -10,8 +12,18 @@ echo Designation- ${designation}
 DATE=$(date +%F)
 echo Hey, Today date is $DATE
 
-ADD=$((2+3+4/5))
-echo ADD - $ADD
 
 ## Variable from command line
-echo b - $b
+echo a - $a
+
+
+ARTH=$((2-3*4/2))
+
+echo ARTH = ${ARTH}
+
+# Special Variables for Inputs
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
